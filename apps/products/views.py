5ad70554bfce80bfcecd4efaef56df67ad9daf9f
WanerValencia2019 <valenciawaner@gmail.com>
2020-07-22 16:29:14 -0500

    add View ProductDetail

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from .models import Product
 # Create your views here.
 
 class Home(ListView):
     model = Product
-    template_name = 'store/home.html'
+    template_name = 'products/home.html'
     context_object_name = 'products'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
         context['news']=Product.objects.get_news()
         print(Product.objects.get_news())
         return context
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name= 'products/detail.html'
