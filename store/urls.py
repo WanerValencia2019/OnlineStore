d3d70497221105977ba113ba539f7d200f1762c3
WanerValencia2019 <valenciawaner@gmail.com>
2020-07-22 16:29:58 -0500

    config static media

--- store/urls.py
+++ store/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
 
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('apps.users.urls'),name='account'),
     path('store/',include('apps.products.urls'),name='products')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
