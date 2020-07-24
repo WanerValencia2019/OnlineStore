28acc529c4ce5559f255771d6477983101922029
WanerValencia2019 <valenciawaner@gmail.com>
2020-07-23 22:49:12 -0500

    add search view, store view

--- apps/products/views.py
+++ apps/products/views.py
@@ -8,12 +8,42 @@ class Home(ListView):
     template_name = 'products/home.html'
     context_object_name = 'products'
 
+    def get_queryset(self):
+        queryset = Product.objects.all()[:10]
+        return queryset
+    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['news']=Product.objects.get_news()
+        context['news']=Product.objects.get_news()[:6]
         print(Product.objects.get_news())
         return context
+
+class StoreView(ListView):
+    queryset = Product.objects.all()  
+    template_name = 'products/store.html'
+    context_object_name = 'products'
+
 
 class ProductDetail(DetailView):
     model = Product
     template_name= 'products/detail.html'
+
+class SearchProductView(ListView):
+    model = Product
+    template_name = 'products/searchResult.html'
+    context_object_name = 'products'
+
+
+    def get_queryset(self):
+        queryset = Product.objects.filter(title__icontains=self.request.GET['search'])
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["query"] = self.request.GET['search'] 
+        return context
+    
+
+
+
