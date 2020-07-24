f8bd8b6f44e56c0e028e5297aa90ac72212ac91d
WanerAndresVM <65208202+WanerAndresVM@users.noreply.github.com>
2020-07-24 17:02:10 -0500

    add search products by category (#4)
    Co-authored-by: WanerValencia2019 <valenciawaner@gmail.com>

--- apps/category/admin.py
+++ apps/category/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Category
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    filter_horizontal = ('products',)
+
+admin.site.register(Category,CategoryAdmin)
