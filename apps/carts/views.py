331e2f554f033d13236575429a6b28a697bcea68
Waner Andrés Valencia Moreno <65208202+WanerAndresVM@users.noreply.github.com>
2020-07-26 11:47:08 -0500

    Cart view (#16)
    * add search products by category
    
    * done categories sideBar store
    
    * add filter by price
    
    * create application cart y model cart with trough table relationed to products
    
    * new view cart
    
    Co-authored-by: WanerValencia2019 <valenciawaner@gmail.com>

--- apps/carts/views.py
+++ apps/carts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
+from .shortcuts import get_or_create_cart
 # Create your views here.
+def cart(request):
+    cart = get_or_create_cart(request)
+
+    return render(request,'carts/cart.html',{'cart':cart})
