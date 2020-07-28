050778a1e7cbf0659acd622fa15646ad827950cb
WanerValencia2019 <valenciawaner@gmail.com>
2020-07-27 22:55:21 -0500

    calc price to pay

--- apps/carts/models.py
+++ apps/carts/models.py
@@ -1,15 +1,18 @@
+import decimal
 import uuid
 from django.db import models
-from django.db.models.signals import pre_save, m2m_changed
+from django.db.models.signals import pre_save, m2m_changed,post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 
 
 
+from .managers import CartProductsManager
 from apps.products.models import Product
 # Create your models here.
 
 UserModel = get_user_model()
+
 
 class Cart(models.Model):
     cart_id = models.CharField(max_length=100,null=False)
@@ -18,6 +21,8 @@ class Cart(models.Model):
     subtotal =  models.DecimalField(max_digits=10,decimal_places=2,default=0.0)
     total = models.DecimalField(max_digits=10,decimal_places=2,default=0.0)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    DISCOUNT = 0.3
 
     def __str__(self):
         return "{}".format(self.cart_id)
@@ -30,21 +35,55 @@ class Cart(models.Model):
     def countProducts(self):
         return self.cartproducts_set.select_related('product').count()
 
+    
+    def updatePrice(self):
+        self.updateSubtotal()
+        self.updateTotal()
+
+    def updateSubtotal(self):
+        self.subtotal = sum([(c.product.price * c.quantity)  for c in self.cartproducts_set.select_related('product')])
+        self.save()
+
+    def updateTotal(self):
+        self.total = self.subtotal - self.get_discount()
+        self.save()
+
+    def get_discount(self):
+        discount = self.subtotal * decimal.Decimal(self.DISCOUNT)
+        return discount.quantize(decimal.Decimal('1.00'))
 
 
-@receiver(pre_save,sender=Cart)
-
-def set_cart_id(instance,*args, **kwargs):
-    if not instance.cart_id:
-        instance.cart_id = str(uuid.uuid4())
 
 class CartProducts(models.Model):
     cart = models.ForeignKey(Cart,on_delete=models.CASCADE)
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
+    objects = CartProductsManager()
 
 
     def __str__(self):
         return "{}".format(self.cart.cart_id)
+
+
+@receiver(pre_save,sender=Cart)
+def set_cart_id(instance,*args, **kwargs):
+    if not instance.cart_id:
+        instance.cart_id = str(uuid.uuid4())
+
+
+@receiver(post_save,sender=CartProducts)
+def updateQuantity(instance,*args, **kwargs):
+    instance.cart.updatePrice()
+
+
+@receiver(m2m_changed,sender=Cart.products.through)
+def set_price(sender,instance,action,*args, **kwargs):
+    print(action)
+    if action=='post_add' or action=='post_remove' or action=='post_clear':
+        instance.updatePrice()
+
+
+
+
 
